c84e0a5d6836896056786d967a2dd2b8e556cbc0
Manik Batra <78477288+Manik3011@users.noreply.github.com>
2023-05-19 14:44:11 +0530

    Add files via upload

---
+++ WebsiteResumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.website_resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteResumeApplication.class, args);
+	}
+
+}
